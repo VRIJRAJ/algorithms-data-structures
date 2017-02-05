a0c819e3902666593669efcf8812344340f8ae55
Dominick Triola <dominick@ignovadesign.com>
2017-02-04 18:38:24 -0800

    create hashing functions

--- specs/sets_maps_cache/lib/p02_hashing.rb
+++ specs/sets_maps_cache/lib/p02_hashing.rb
@@ -1,21 +1,35 @@
 class Fixnum
-  # Fixnum#hash already implemented for you
+  # Use Ruby's default Fixnum#hash
 end
 
 class Array
   def hash
+    duped = self.dup
+    hash_int = duped.shift.hash
+    duped.each_with_index do |el, index|
+      hash_int ^= (el + index).hash
+    end
+
+    hash_int
   end
 end
 
 class String
   def hash
+    chars = self.chars.map(&:ord)
+    chars.each_with_index.inject(0) do |hash_int, (ord, index)|
+      hash_int ^ (ord + index).hash
+    end
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_int = 0
+    self.each do |key, value|
+      hash_int ^= (key.hash + value.hash)
+    end
+
+    hash_int
   end
 end
