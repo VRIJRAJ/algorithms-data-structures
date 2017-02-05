55a2e04b7cdec220b0359200b88b026fd3856a3a
Dominick Triola <dominick@ignovadesign.com>
2017-02-04 18:06:46 -0800

    create int sets

--- specs/sets_maps_cache/lib/p01_int_set.rb
+++ specs/sets_maps_cache/lib/p01_int_set.rb
@@ -31,25 +31,28 @@ class MaxIntSet
 end
 
 
-# Store integers using buckets
+# Store integers using a set amount of buckets
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num] << num unless include?(num)
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -68,18 +71,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless include?(num)
+      resize! if @count == num_buckets
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -87,5 +100,8 @@ class ResizingIntSet
   end
 
   def resize!
+    items = @store.reduce { |accum, bucket| accum + bucket }
+    initialize(num_buckets * 2)
+    items.each { |item| insert(item) }
   end
 end
