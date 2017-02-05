8e1b30766cfd31540512f82831f8a81da5bd10d2
Dominick Triola <dominick@ignovadesign.com>
2017-02-04 19:34:34 -0800

    create linked list

---
+++ data_structures/linked_list/link.rb
@@ -0,0 +1,19 @@
+class Link
+  attr_accessor :key, :val, :next, :prev
+
+  def initialize(key = nil, val = nil)
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@key}: #{@val}"
+  end
+
+  def remove
+    @prev.next = @next
+    @next.prev = @prev
+  end
+end
