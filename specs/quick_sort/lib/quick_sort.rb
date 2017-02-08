4df1cb5c5e18105254b9a1319cc6756de22607a1
Dominick Triola <dominick@ignovadesign.com>
2017-02-07 23:42:35 -0800

    pass quick-sort specs

--- specs/quick_sort/lib/quick_sort.rb
+++ specs/quick_sort/lib/quick_sort.rb
@@ -16,8 +16,32 @@ class QuickSort
 
   # In-place.
   def self.sort2!(array, start = 0, length = array.length, &prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+
+    return array if length < 2
+
+    pos = partition(array, start, length, &prc)
+
+    sort2!(array, start, pos - start, &prc)
+    sort2!(array, pos + 1, length - pos - 1, &prc)
+
+    array
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+
+    pos = start
+    el = array[start]
+
+    ((start + 1)...(start + length)).each do |j|
+      if prc.call(el, array[j]) > 0
+        array[j], array[pos + 1] = array[pos + 1], array[j]
+        pos += 1
+      end
+    end
+
+    array[start], array[pos] = array[pos], array[start]
+    pos
   end
 end
