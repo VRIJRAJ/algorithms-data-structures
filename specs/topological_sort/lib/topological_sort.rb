20d66817aa26b074e8ec84252efb6df760c4b82b
Dominick Triola <dominick@ignovadesign.com>
2017-02-14 23:10:13 -0800

    pass topological sort specs

--- specs/topological_sort/lib/topological_sort.rb
+++ specs/topological_sort/lib/topological_sort.rb
@@ -1,7 +1,57 @@
 require_relative 'graph'
 
-# Implementing topological sort using both Khan's and Tarian's algorithms
+# Look for vertices with no in_edges to do first
+# Then delete all edges coming out of those vertices
+# Repeat until there are no vertices left
 
+# Khan's
+# Queue all vertices with no in_edges
+# Remove queued vertices and their edges from graph
+# Push vertex into results array
+# Look at each destination node and push onto queue if it has no in_edges
 def topological_sort(vertices)
+  result = []
+  queue = []
+  edge_counts = {}
 
+  vertices.each do |vertex|
+    edge_counts[vertex] = vertex.in_edges.count
+    queue << vertex if vertex.in_edges.empty?
+  end
+
+  until queue.empty?
+    vertex = queue.shift
+    result << vertex
+
+    vertex.out_edges.each do |edge|
+      edge_counts[edge.to_vertex] -= 1
+      queue << edge.to_vertex if edge_counts[edge.to_vertex] == 0
+    end
+  end
+
+  result
+end
+
+# Tarian's
+def topological_sort(vertices)
+  result = []
+  visited = Set.new
+
+  vertices.each do |vertex|
+    dfs!(vertex, visited, result) unless visited.include?(vertex)
+  end
+
+  result
+end
+
+def dfs!(vertex, visited, result)
+  visited.add(vertex)
+
+  vertex.out_edges.each do |edge|
+    unless visited.include?(edge.to_vertex)
+      dfs!(edge.to_vertex, visited, result)
+    end
+  end
+
+  result.unshift(vertex)
 end
